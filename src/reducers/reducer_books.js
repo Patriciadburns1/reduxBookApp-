743379e48ca45076014fa7279a176ad08d7bfc90
dburns1 <patriciadburns@gmail.com>
2018-10-01 18:23:08 -0700

    init commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function (){
+    return
+        [ 
+{title: 'JavaScript the Good Parts'},
+{title: 'Harry Potter'},
+{title: "Dark Tower"}, 
+{title: 'Eloquent Ruby'}
+        ]
+
+}
